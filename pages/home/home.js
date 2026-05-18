8307b3b8a87620738fdd4807b4df5b812a400f94
Developer <dev@example.com>
2026-05-18 09:07:10 +0000

    fix: 完全重构移除Canvas，使用CSS图表解决超时问题
    - home.js: 移除Canvas初始化，只保留数据和简单交互
    - home.wxml: 移除canvas标签，使用纯CSS图表
    - home.wxss: 添加CSS图表样式（柱状图）
    - 删除utils/chart.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -10,6 +10,15 @@ Page({
       { name: '平均转化成本', value: '217', change: '环比 22.5% ↑' },
       { name: '展示数', value: '4,555', change: '环比 10.0% ↑' },
       { name: '点击数', value: '123', change: '环比 5.2% ↓' }
+    ],
+    chartData: [
+      { label: '周一', consume: 12, convert: 2 },
+      { label: '周二', consume: 18, convert: 3 },
+      { label: '周三', consume: 15, convert: 2 },
+      { label: '周四', consume: 22, convert: 4 },
+      { label: '周五', consume: 28, convert: 5 },
+      { label: '周六', consume: 24, convert: 4 },
+      { label: '周日', consume: 30, convert: 6 }
     ]
   },
 
@@ -20,109 +29,12 @@ Page({
     }
   },
 
-  onReady: function() {
-    this.initChart();
-  },
-
-  initChart: function() {
-    var that = this;
-    var query = wx.createSelectorQuery();
-    query.select('#homeChart').fields({ node: true, size: true }).exec(function(res) {
-      if (!res[0]) return;
-      
-      var canvas = res[0].node;
-      var ctx = canvas.getContext('2d');
-      var dpr = wx.getSystemInfoSync().pixelRatio;
-      
-      canvas.width = res[0].width * dpr;
-      canvas.height = res[0].height * dpr;
-      ctx.scale(dpr, dpr);
-      
-      that.canvasCtx = ctx;
-      that.canvasWidth = res[0].width;
-      that.canvasHeight = res[0].height;
-      
-      that.drawChart('consume');
-    });
-  },
-
-  drawChart: function(type) {
-    if (!this.canvasCtx) return;
-    
-    var ctx = this.canvasCtx;
-    var width = this.canvasWidth;
-    var height = this.canvasHeight;
-    var left = 35, top = 15, bottom = 25, right = 10;
-    var chartW = width - left - right;
-    var chartH = height - top - bottom;
-    
-    ctx.clearRect(0, 0, width, height);
-    
-    ctx.strokeStyle = '#E8EBF0';
-    ctx.lineWidth = 1;
-    for (var i = 0; i <= 3; i++) {
-      var y = top + (chartH / 3) * i;
-      ctx.beginPath();
-      ctx.moveTo(left, y);
-      ctx.lineTo(width - right, y);
-      ctx.stroke();
-    }
-    
-    ctx.fillStyle = '#999';
-    ctx.font = '9px sans-serif';
-    ctx.textAlign = 'right';
-    for (var j = 0; j <= 3; j++) {
-      ctx.fillText((3 - j) * 15 + '', left - 3, top + (chartH / 3) * j + 3);
-    }
-    
-    ctx.textAlign = 'center';
-    var labels = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
-    var xStep = chartW / 6;
-    for (var k = 0; k < labels.length; k++) {
-      ctx.fillText(labels[k], left + xStep * k, height - 5);
-    }
-    
-    var data = type === 'consume' 
-      ? [12, 18, 15, 22, 28, 24, 30]
-      : [2, 3, 2, 4, 5, 4, 6];
-    var color = type === 'consume' ? '#0066FF' : '#00C853';
-    
-    var maxVal = 30;
-    var minVal = 0;
-    var range = maxVal - minVal || 1;
-    
-    ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    for (var m = 0; m < data.length; m++) {
-      var x = left + xStep * m;
-      var y = top + chartH - ((data[m] - minVal) / range) * chartH;
-      if (m === 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
-      }
-    }
-    ctx.stroke();
-    
-    ctx.fillStyle = color;
-    for (var n = 0; n < data.length; n++) {
-      var px = left + xStep * n;
-      var py = top + chartH - ((data[n] - minVal) / range) * chartH;
-      ctx.beginPath();
-      ctx.arc(px, py, 3, 0, 2 * Math.PI);
-      ctx.fill();
-    }
-  },
-
   selectMetric: function(e) {
     this.setData({ selectedIndex: e.currentTarget.dataset.index });
   },
 
   switchChart: function(e) {
-    var type = e.currentTarget.dataset.type;
-    this.setData({ chartType: type });
-    this.drawChart(type);
+    this.setData({ chartType: e.currentTarget.dataset.type });
   },
 
   goToAccountSelect: function() {

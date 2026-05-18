f41027e78d20f20b8c88e2e3535b2b58f3707238
Developer <dev@example.com>
2026-05-18 07:11:35 +0000

    feat: 更新首页设计 - 新头部布局 + 折线图 + 小屏适配
    - 新版头部：账户名称 + 切换图标（同一行）+ 个人中心（下方小字）
    - 数据卡片缩小：100px宽度，前3个可同时显示
    - 趋势图改为折线图：带坐标轴、网格线、数据点
    - 优化整体布局紧密度，适配小屏机器
    - 新增 switch.png 和 user.png 图标

--- wechat-miniprogram/pages/home/home.js
+++ wechat-miniprogram/pages/home/home.js
@@ -6,12 +6,12 @@ Page({
     selectedMetricIndex: 0,
     chartType: 'consume',
     overviewMetrics: [
-      { name: '有消耗账户数', value: '0', change: '环比 0.00% —' },
-      { name: '消耗(元)', value: '0.00', change: '环比 ---' },
-      { name: '转化数', value: '0', change: '环比 ---' },
-      { name: '平均转化成本', value: '0.00', change: '' },
-      { name: '展示数', value: '0', change: '' },
-      { name: '点击数', value: '0', change: '' }
+      { name: '有消耗账户数', value: '15', change: '环比 12.5% ↑' },
+      { name: '消耗(元)', value: '5,000', change: '环比 15.2% ↑' },
+      { name: '转化数', value: '23', change: '环比 8.3% ↓' },
+      { name: '平均转化成本', value: '217', change: '环比 22.5% ↑' },
+      { name: '展示数', value: '4,555', change: '环比 10.0% ↑' },
+      { name: '点击数', value: '123', change: '环比 5.2% ↓' }
     ]
   },
 
@@ -32,17 +32,6 @@ Page({
     var that = this;
     
     setTimeout(function() {
-      that.setData({
-        overviewMetrics: [
-          { name: '有消耗账户数', value: '15', change: '环比 12.50% ↑' },
-          { name: '消耗(元)', value: '5000.00', change: '环比 15.20% ↑' },
-          { name: '转化数', value: '23', change: '环比 8.30% ↓' },
-          { name: '平均转化成本', value: '217.39', change: '环比 22.50% ↑' },
-          { name: '展示数', value: '4555', change: '环比 10.00% ↑' },
-          { name: '点击数', value: '123', change: '环比 5.20% ↓' }
-        ]
-      });
-      
       that.initChart();
     }, 300);
   },
@@ -64,25 +53,26 @@ Page({
     
     var chartData = {
       consume: {
-        data: [120, 200, 180, 320, 450, 380, 520],
-        color: '#0066FF'
+        data: [12, 18, 15, 22, 28, 24, 30],
+        color: '#0066FF',
+        yAxis: { data: ['30', '25', '20', '15', '10', '5', '0'] }
       },
       convert: {
         data: [2, 3, 2, 4, 5, 4, 6],
-        color: '#00C853'
+        color: '#00C853',
+        yAxis: { data: ['6', '5', '4', '3', '2', '1', '0'] }
       }
     };
     
     var data = chartData[type] || chartData.consume;
     
     this.chartInstance.setOption({
-      grid: { left: 50, right: 20, top: 20, bottom: 30 },
       xAxis: { data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'] },
-      yAxis: {},
+      yAxis: data.yAxis,
       series: [{
         type: 'line',
         data: data.data,
-        lineStyle: { color: data.color, width: 3 },
+        lineStyle: { color: data.color, width: 2 },
         areaStyle: { color: data.color }
       }]
     });
@@ -99,7 +89,7 @@ Page({
 
   switchChartTab: function(e) {
     var type = e.currentTarget.dataset.type;
-    this.drawChart(type);
+    this.drawChart(type === 'consume' ? 'consume' : 'convert');
   },
 
   goToAccountSelect: function() {
--- wechat-miniprogram/utils/chart.js
+++ wechat-miniprogram/utils/chart.js
@@ -5,128 +5,135 @@ var chartModule = {
       return null;
     }
     
+    var context = null;
     try {
-      var context = wx.createCanvasContext(canvasId, page);
-      
-      return {
-        context: context,
-        setOption: function(option) {
-          if (!option) return;
-          
-          var ctx = this.context;
-          if (!ctx) return;
-          
-          var grid = option.grid;
-          var xAxis = option.xAxis;
-          var yAxis = option.yAxis;
-          var series = option.series;
-          
-          var defaultWidth = 320;
-          var defaultHeight = 200;
-          var padding = {
-            left: (grid && grid.left) ? grid.left : 40,
-            right: (grid && grid.right) ? grid.right : 15,
-            top: (grid && grid.top) ? grid.top : 15,
-            bottom: (grid && grid.bottom) ? grid.bottom : 25
-          };
-          
-          var chartWidth = defaultWidth - padding.left - padding.right;
-          var chartHeight = defaultHeight - padding.top - padding.bottom;
-          
-          ctx.clearRect(0, 0, defaultWidth, defaultHeight);
-          
-          ctx.setStrokeStyle('#E8EBF0');
-          ctx.setLineWidth(1);
-          for (var i = 0; i <= 3; i++) {
-            var y = padding.top + (chartHeight / 3) * i;
-            ctx.beginPath();
-            ctx.moveTo(padding.left, y);
-            ctx.lineTo(defaultWidth - padding.right, y);
-            ctx.stroke();
-          }
-          
-          ctx.setFillStyle('#999999');
-          ctx.setFontSize(10);
-          ctx.setTextAlign('right');
-          
-          if (yAxis && yAxis.data && yAxis.data.length > 0) {
-            var yValues = yAxis.data;
-            for (var j = 0; j <= 3; j++) {
-              var value = yValues[3 - j] || '';
-              var yPos = padding.top + (chartHeight / 3) * j;
-              ctx.fillText(value.toString(), padding.left - 5, yPos + 4);
-            }
-          }
-          
-          ctx.setTextAlign('center');
-          if (xAxis && xAxis.data && xAxis.data.length > 0) {
-            var xValues = xAxis.data;
-            var xStep = Math.ceil(xValues.length / 5);
-            for (var k = 0; k < xValues.length; k++) {
-              if (k % xStep === 0 || k === xValues.length - 1) {
-                var xPos = padding.left + (chartWidth / (xValues.length - 1)) * k;
-                ctx.fillText(xValues[k].toString(), xPos, defaultHeight - padding.bottom + 15);
-              }
-            }
-          }
-          
-          if (series && series.length > 0) {
-            for (var s = 0; s < series.length; s++) {
-              var serie = series[s];
-              if (!serie.data) continue;
-              
-              var data = serie.data;
-              var lineColor = (serie.lineStyle && serie.lineStyle.color) ? serie.lineStyle.color : '#0066FF';
-              var lineWidth = (serie.lineStyle && serie.lineStyle.width) ? serie.lineStyle.width : 2;
-              
-              var minVal = Math.min.apply(null, data);
-              var maxVal = Math.max.apply(null, data);
-              var range = maxVal - minVal;
-              
-              ctx.beginPath();
-              for (var m = 0; m < data.length; m++) {
-                var xCoord = padding.left + (chartWidth / (data.length - 1)) * m;
-                var yCoord;
-                if (range > 0) {
-                  yCoord = padding.top + chartHeight - ((data[m] - minVal) / range) * chartHeight;
-                } else {
-                  yCoord = padding.top + chartHeight / 2;
-                }
-                
-                if (m === 0) {
-                  ctx.moveTo(xCoord, yCoord);
-                } else {
-                  ctx.lineTo(xCoord, yCoord);
-                }
-              }
-              ctx.setStrokeStyle(lineColor);
-              ctx.setLineWidth(lineWidth);
-              ctx.stroke();
-              
-              ctx.setFillStyle(lineColor);
-              for (var n = 0; n < data.length; n++) {
-                var xDot = padding.left + (chartWidth / (data.length - 1)) * n;
-                var yDot;
-                if (range > 0) {
-                  yDot = padding.top + chartHeight - ((data[n] - minVal) / range) * chartHeight;
-                } else {
-                  yDot = padding.top + chartHeight / 2;
-                }
-                
-                ctx.beginPath();
-                ctx.arc(xDot, yDot, 3, 0, 2 * Math.PI);
-                ctx.fill();
-              }
-            }
-          }
-          
-          ctx.draw();
-        }
-      };
+      context = wx.createCanvasContext(canvasId, page);
     } catch (e) {
-      console.error('Chart init error:', e);
+      console.error('Canvas context error:', e);
       return null;
     }
+    
+    return {
+      context: context,
+      setOption: function(option) {
+        if (!option || !context) return;
+        
+        var ctx = context;
+        var width = 320;
+        var height = 320;
+        
+        var padding = {
+          left: 50,
+          right: 20,
+          top: 20,
+          bottom: 40
+        };
+        
+        var chartWidth = width - padding.left - padding.right;
+        var chartHeight = height - padding.top - padding.bottom;
+        
+        ctx.clearRect(0, 0, width, height);
+        
+        ctx.setStrokeStyle('#E8EBF0');
+        ctx.setLineWidth(1);
+        var gridCount = 5;
+        for (var i = 0; i <= gridCount; i++) {
+          var y = padding.top + (chartHeight / gridCount) * i;
+          ctx.beginPath();
+          ctx.moveTo(padding.left, y);
+          ctx.lineTo(width - padding.right, y);
+          ctx.stroke();
+        }
+        
+        var xAxis = option.xAxis;
+        var yAxis = option.yAxis;
+        var series = option.series;
+        
+        ctx.setFillStyle('#999999');
+        ctx.setFontSize(10);
+        ctx.setTextAlign('right');
+        
+        if (yAxis && yAxis.data && yAxis.data.length > 0) {
+          var yValues = yAxis.data;
+          for (var j = 0; j <= gridCount; j++) {
+            var value = yValues[gridCount - j] || '';
+            var yPos = padding.top + (chartHeight / gridCount) * j;
+            ctx.fillText(value.toString(), padding.left - 8, yPos + 4);
+          }
+        } else {
+          for (var g = 0; g <= gridCount; g++) {
+            var gyPos = padding.top + (chartHeight / gridCount) * g;
+            ctx.fillText((gridCount - g) * 5 + '', padding.left - 8, gyPos + 4);
+          }
+        }
+        
+        ctx.setTextAlign('center');
+        if (xAxis && xAxis.data && xAxis.data.length > 0) {
+          var xValues = xAxis.data;
+          var step = Math.ceil(xValues.length / 5);
+          for (var k = 0; k < xValues.length; k++) {
+            if (k % step === 0 || k === xValues.length - 1) {
+              var xPos = padding.left + (chartWidth / (xValues.length - 1)) * k;
+              ctx.fillText(xValues[k].toString(), xPos, height - padding.bottom + 18);
+            }
+          }
+        }
+        
+        if (series && series.length > 0) {
+          for (var s = 0; s < series.length; s++) {
+            var serie = series[s];
+            if (!serie.data) continue;
+            
+            var data = serie.data;
+            var lineColor = (serie.lineStyle && serie.lineStyle.color) ? serie.lineStyle.color : '#0066FF';
+            
+            var minVal = Math.min.apply(null, data);
+            var maxVal = Math.max.apply(null, data);
+            var range = maxVal - minVal || 1;
+            
+            var points = [];
+            for (var m = 0; m < data.length; m++) {
+              var xCoord = padding.left + (chartWidth / (data.length - 1)) * m;
+              var yCoord = padding.top + chartHeight - ((data[m] - minVal) / range) * chartHeight;
+              points.push({ x: xCoord, y: yCoord });
+            }
+            
+            ctx.beginPath();
+            ctx.moveTo(points[0].x, points[0].y);
+            for (var p = 1; p < points.length; p++) {
+              ctx.lineTo(points[p].x, points[p].y);
+            }
+            ctx.setStrokeStyle(lineColor);
+            ctx.setLineWidth(2);
+            ctx.stroke();
+            
+            if (serie.areaStyle) {
+              ctx.lineTo(points[points.length - 1].x, height - padding.bottom);
+              ctx.lineTo(points[0].x, height - padding.bottom);
+              ctx.closePath();
+              ctx.setFillStyle('rgba(0, 102, 255, 0.1)');
+              ctx.fill();
+            }
+            
+            ctx.setFillStyle(lineColor);
+            for (var n = 0; n < points.length; n++) {
+              ctx.beginPath();
+              ctx.arc(points[n].x, points[n].y, 4, 0, 2 * Math.PI);
+              ctx.fill();
+              
+              ctx.setFillStyle('#FFFFFF');
+              ctx.beginPath();
+              ctx.arc(points[n].x, points[n].y, 2, 0, 2 * Math.PI);
+              ctx.fill();
+              
+              ctx.setFillStyle(lineColor);
+            }
+          }
+        }
+        
+        ctx.draw();
+      }
+    };
   }
 };
 
